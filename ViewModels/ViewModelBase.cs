02252c4122506fc107a14aa087a567ad84ea902d
CauanEDP <cauannunes0464@gmail.com>
2026-02-26 16:17:22 -0300

    Alteração no ViewModelBase

--- ViewModels/ViewModelBase.cs
+++ ViewModels/ViewModelBase.cs
@@ -5,9 +5,9 @@ namespace Primeiro_Projeto.ViewModels
 {
     public class ViewModelBase : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler? PropertyChanged;      // Estudar para que serve o "?" nesse código
+        public event PropertyChangedEventHandler? PropertyChanged;     
 
-        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)  
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)    
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
